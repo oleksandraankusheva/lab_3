9f181cc1db1be0204b72d35cb32716dbf09121f7
oleksandraankusheva <119884196+oleksandraankusheva@users.noreply.github.com>
2023-12-22 13:46:43 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Program
+{
+    static int ReadNumberFromFile(string fileName, int lineNumber)
+    {
+        
+        string line = File.ReadLines(fileName).ElementAt(lineNumber - 1);
+        
+        return Convert.ToInt32(line);
+    }
+    static void Main(string[] args)
+    {
+        
+        Directory.CreateDirectory("./messages");
+        
+        StreamWriter noFileWriter = new StreamWriter("./messages/no_file.txt");
+        StreamWriter badDataWriter = new StreamWriter("./messages/bad_data.txt");
+        StreamWriter overflowWriter = new StreamWriter("./messages/overflow.txt");
+        
+        int sum = 0;
+        int productCount = 0;
+         
+        for (int i = 10; i <= 29; i++)
+        {
+            string fileName = i.ToString() + ".txt";   
+            try
+            {
+                int firstNumber = ReadNumberFromFile(fileName, 1);
+                int secondNumber = ReadNumberFromFile(fileName, 2);
+                sum += checked(firstNumber * secondNumber);
+                productCount++;
+            }
+            catch (FileNotFoundException)
+            {
+                noFileWriter.WriteLine(fileName);
+            }
+            catch (FormatException)
+            {
+                badDataWriter.WriteLine(fileName);
+            }
+            catch (OverflowException)
+            {
+                overflowWriter.WriteLine(fileName);
+            }
+        }
+        
+        noFileWriter.Close();
+        badDataWriter.Close();
+        overflowWriter.Close();
+
+        Console.WriteLine("Сума добуткiв: " + sum);
+        Console.WriteLine("Середнє арифметичне добуткiв: " + (sum / productCount));
+        Console.ReadLine();
+    }
+    
+}
---
+++ Program2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MirrorImage
+{
+
+    class Program
+    {
+        private static void FlipImageVertical(Bitmap image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+
+            for (int y = 0; y < height / 2; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color temp = image.GetPixel(x, y);
+                    image.SetPixel(x, y, image.GetPixel(x, height - 1 - y));
+                    image.SetPixel(x, height - 1 - y, temp);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            MessageBox.Show("SomeMessage");
+            
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (string fileName in files)
+            {
+                
+                try
+                {
+                    Bitmap newImage = new Bitmap(fileName);
+
+                    FlipImageVertical(newImage);
+
+                    newImage.Save(Path.ChangeExtension(fileName, "-mirrored.gif"));
+                }
+                
+                catch (Exception)
+                {
+                    Console.WriteLine(" Файл '{0}' не є графічним файлом", fileName);
+                }
+            }
+            Console.ReadKey();
+        }      
+    }
+}
